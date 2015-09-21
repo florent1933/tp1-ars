519ef468d638b1d98565094c53f10561947529cb
Florent <clapieflor@eisti.eu>
2015-09-21 10:12:34 +0200

    init files and load files

---
+++ script.R
@@ -0,0 +1,10 @@
+# Load data
+library(igraph)
+
+setwd('~/Documents/DS/tp1-ars/')
+karate <- read.graph("data/karate.gml.txt", format="gml")
+football <- read.graph("data/football.gml.txt", format="gml")
+dolphins <- read.graph("data/dolphins.gml.txt", format="gml")
+polbooks <- read.graph("data/polbooks.gml.txt", format="gml")
+
+
